ab5160d33773b869d785a4962af737c916c0a5f9
Michal Czeraszkiewicz <czerasz.hosting@gmail.com>
2015-03-02 22:51:02 +0100

    Return information about the requested path

--- app.rb
+++ app.rb
@@ -3,12 +3,14 @@
 require 'sinatra'
 
 get '/*' do
-    out = "<h1>Current Time Is</h1>"
+    out = "<h1>Current Time Is</h1>" + "\n"
 
     # Get current time
     now = Time.now
-    
+    current_path = request.fullpath
+
     out += '<p>'+ now.strftime("%Y.%d.%m %H:%M:%S") +'</p>' + "\n"
+    out += '<p>'+ current_path +'</p>' + "\n"
 
     return out
 end
